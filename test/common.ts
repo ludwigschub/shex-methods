090538ffc824ec8895498926dcef62fd96042ea9
Ludwig Schubert <ludwigschubi1@gmail.com>
2021-05-23 12:32:39 +0200

    Fix test config

--- test/common.ts
+++ test/common.ts
@@ -3,6 +3,12 @@ import * as pathUtils from 'path';
 const config = require('dotenv').config().parsed;
 
 export const podUrl = (path: string) => {
-  const host = new URL(config.SOLID_IDP).host;
-  return 'https://' + pathUtils.join(`${config.SOLID_USERNAME}.${host}`, path);
+  const host = new URL(config.SOLID_IDP ?? process.env.SOLID_IDP).host;
+  return (
+    'https://' +
+    pathUtils.join(
+      `${config.SOLID_USERNAME ?? process.env.SOLID_USERNAME}.${host}`,
+      path,
+    )
+  );
 };
