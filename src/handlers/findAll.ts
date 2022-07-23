43cd4cd2893b629f8d778b2378ccfe9e5756d548
ludwigschubi <ludwigschubi1@gmail.com>
2022-07-23 22:32:26 +0200

    feat: add req options

--- src/handlers/findAll.ts
+++ src/handlers/findAll.ts
@@ -1,3 +1,5 @@
+import { AutoInitOptions } from 'rdflib';
+
 import { QueryResult, Shape } from '../shape';
 import { validateShapes } from '../validate';
 
@@ -9,10 +11,12 @@ export interface FindAllArgs<ShapeType> {
 export async function findAll<ShapeType, CreateShapeArgs>(
   shape: Shape<ShapeType, CreateShapeArgs>,
   { where, doc }: FindAllArgs<ShapeType>,
+  reqOptions?: Partial<AutoInitOptions>
 ): Promise<QueryResult<ShapeType[]>> {
   const ids = where?.id;
   await shape.fetcher.load(doc, {
     headers: new Headers({ Accept: 'text/turtle' }),
+    ...(reqOptions ?? {})
   });
   const [data, errors] = await validateShapes<ShapeType, CreateShapeArgs>(
     shape,
--- src/handlers/findOne.ts
+++ src/handlers/findOne.ts
@@ -1,3 +1,5 @@
+import { AutoInitOptions } from 'rdflib';
+
 import { QueryResult, Shape } from '../shape';
 import { validateShapes } from '../validate';
 
@@ -9,10 +11,12 @@ export interface FindUniqueArgs {
 export async function findOne<ShapeType, CreateShapeArgs>(
   shape: Shape<ShapeType, CreateShapeArgs>,
   { where, doc }: FindUniqueArgs,
+  reqOptions?: Partial<AutoInitOptions>
 ): Promise<QueryResult<ShapeType>> {
   const { id } = where;
   await shape.fetcher.load(doc, {
     headers: new Headers({ Accept: 'text/turtle' }),
+    ...(reqOptions ?? {})
   });
   const [data, errors] = await validateShapes<ShapeType, CreateShapeArgs>(
     shape,
